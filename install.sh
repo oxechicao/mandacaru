7d466d5ac3f70a9b74e14d4dc82ce1e8c45dd592
Chicão Thiago <fthiagogv+github@gmail.com>
2026-02-07 15:38:29 -0300

    script para instalar pacotes no mac

---
+++ install.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+function macos_requirements() {
+  brew install zsh-syntax-hightling
+}
+
+echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >>$HOME/.zshrc
