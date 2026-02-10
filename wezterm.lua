0bd0532e043538a1229535abe67a13c287de9a30
TVieira <thiago_vieira@gap.com>
2026-02-10 18:48:50 -0300

    adicionandno transparencia

--- wezterm.lua
+++ wezterm.lua
@@ -8,6 +8,8 @@ config.max_fps = 100
 config.prefer_egl = true
 config.enable_tab_bar = false
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 100
 
 -- Editor
 config.font_size = 16
