0feeae80efaa3041fde5bb4c53287224a6219632
TVieira <thiago_vieira@gap.com>
2026-02-11 12:11:41 -0300

    feat: adicionando submodules
    Adicionando submodule para o lazyvim
    Corrigido o comando para configurar o editor do git.

--- install.sh
+++ install.sh
@@ -11,12 +11,20 @@ echo "Adding zsh-syntax-highlighting to .zshrc"
 echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >>$HOME/.zshrc
 
 echo "Setup Wezterm conf"
-mv $HOME/.wezterm.lua{,.bkp}
+mv $HOME/.wezterm.lua{,.mandacaru_bkp}
 ln -s $(pwd)/wezterm.lua $HOME/.wezterm.lua
 
 echo "Setup Tmux conf"
-mv $HOME/.temux.conf{,.bkp}
+mv $HOME/.temux.conf{,.mandacaru_bkp}
 ln -s $(pwd)/temux.conf $HOME/.tmux.conf
 
 echo "Setup git config global"
-git config --global core.editor = "nvim"
+git config --global core.editor "nvim"
+
+echo "Initializin submodules"
+git submodule init
+git submodule update
+
+echo "Setup nvim with lazyvim"
+mv $HOME/.config/nvim{,.mandacaru_bkp}
+ln -s $(pwd)/lazyvim $HOME/.config/nvim
