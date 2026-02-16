4caa4d318211bd5f891acebf6f2e172e6ba180b9
TVieira <thiago_vieira@gap.com>
2026-02-16 14:44:11 -0300

    submodule update:

--- install.sh
+++ install.sh
@@ -4,27 +4,82 @@ function macos_requirements() {
   brew install zsh-syntax-hightling tmux wezterm nvim
 }
 
-echo "Installing dependencies"
-macos_requirements
+function install_deps() {
+  echo "Installing dependencies"
+  macos_requirements
+  echo "Adding zsh-syntax-highlighting to .zshrc"
+  echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >>$HOME/.zshrc
+}
 
-echo "Adding zsh-syntax-highlighting to .zshrc"
-echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >>$HOME/.zshrc
+function setup_wez() {
+  echo "Setup Wezterm conf"
+  mv $HOME/.wezterm.lua{,.mandacaru_bkp}
+  ln -s $(pwd)/wezterm.lua $HOME/.wezterm.lua
+}
 
-echo "Setup Wezterm conf"
-mv $HOME/.wezterm.lua{,.mandacaru_bkp}
-ln -s $(pwd)/wezterm.lua $HOME/.wezterm.lua
+function setup_tmux() {
+  echo "Setup Tmux conf"
+  mv $HOME/.temux.conf{,.mandacaru_bkp}
+  ln -s $(pwd)/temux.conf $HOME/.tmux.conf
+}
 
-echo "Setup Tmux conf"
-mv $HOME/.temux.conf{,.mandacaru_bkp}
-ln -s $(pwd)/temux.conf $HOME/.tmux.conf
+function setup_git() {
+  echo "Setup git config global"
+  git config --global core.editor "nvim"
+}
 
-echo "Setup git config global"
-git config --global core.editor "nvim"
+function setup_lazyvim() {
+  echo "Initializin submodules"
+  git submodule init
+  git submodule update
+  echo "Setup nvim with lazyvim" mv $HOME/.config/nvim{,.mandacaru_bkp}
+  ln -s $(pwd)/lazyvim $HOME/.config/nvim
+}
 
-echo "Initializin submodules"
-git submodule init
-git submodule update
+function setup_mandacarurc() {
+  echo "source $(pwd)/mandacarurc.sh" >>$HOME/.zshrc
+}
 
-echo "Setup nvim with lazyvim"
-mv $HOME/.config/nvim{,.mandacaru_bkp}
-ln -s $(pwd)/lazyvim $HOME/.config/nvim
+function install_everything() {
+  install_deps
+  setup_wez
+  setup_tmux
+  setup_git
+  setup_lazyvim
+  setup_mandacarurc
+}
+
+option=$1
+
+case $option in
+"0")
+  setup_everything
+  ;;
+"1")
+  install_deps
+  ;;
+"2")
+  setup_wez
+  ;;
+"3")
+  setup_tmux
+  ;;
+"4")
+  setup_git
+  ;;
+"5")
+  setup_lazyvim
+  ;;
+"6")
+  setup_mandacarurc
+  ;;
+*)
+  echo "0 - Install everything"
+  echo "1 - Install dependencies"
+  echo "2 - Setup Wezterm"
+  echo "3 - Setup Tmux"
+  echo "4 - Setup Git"
+  echo "5 - Setup LazyVim"
+  echo "6 - Setup Mandacarurc"
+  ;;
+esac
--- mandacarurc.sh
+++ mandacarurc.sh
@@ -6,38 +6,57 @@
 
 apps=(chrome brave wezterm nvim)
 function cpu_app() {
-	app=$1
-	ps -eo pcpu,command | grep -i $app | awk '{p=$1 ; sum +=p} END {print sum "%"}'
+  app=$1
+  ps -eo pcpu,command | grep -i $app | awk '{p=$1 ; sum +=p} END {print sum "%"}'
 }
 
 function mem_app() {
-	app=$1
-	ps -eo rss,command | grep -i $app | awk '{m=$1 ; sum +=m} END {print sum/1024 " MB"}'
+  app=$1
+  ps -eo rss,command | grep -i $app | awk '{m=$1 ; sum +=m} END {print sum/1024 " MB"}'
 }
 
 function resource_app() {
-	app=$1
-	cpu=$(cpu_app $app)
-	mem=$(mem_app $app)
-	echo "$app|$cpu|$mem"
+  app=$1
+  cpu=$(cpu_app $app)
+  mem=$(mem_app $app)
+  echo "$app|$cpu|$mem"
 }
 
 function generate_report() {
-	echo "App|CPU|Mem"
-	for app in "${apps[@]}"; do
-		resource_app "$app"
-	done
+  echo "App|CPU|Mem"
+  for app in "${apps[@]}"; do
+    resource_app "$app"
+  done
 }
 
 function consume_apps() {
-	generate_report | column -s '|' -t
+  generate_report | column -s '|' -t
 }
 
 function wca() {
-	time=$1
-	while true; do
-		clear
-		consume_apps
-		sleep $1
-	done
+  time=$1
+  while true; do
+    clear
+    consume_apps
+    sleep $1
+  done
+}
+
+function config_git() {
+  git config user.name "Chicão Thiago"
+  git config user.email "fthiagogv+github@gmail.com"
+}
+
+function mandacaru_sm_up() {
+  echo "Updating lazyvim submodule..."
+  dir=$(pwd)
+  msg=$1
+  git add .
+  git commit -m "$msg"
+  git push origin main
+  cd ..
+  git add .
+  git commit -m "submodule update: $msg"
+  git push origin main
+  cd $dir
 }
